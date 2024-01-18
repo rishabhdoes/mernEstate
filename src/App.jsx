922cee41b59bb8b2be63e03917f49414f6898a25
shubham <girishubham196@gmail.com>
2024-01-19 01:28:29 +0530

    DATABASE SETUP

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Home from './pages/Home'
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import Header from './components/Header';
 console.log(React);
 const App = () => {
   return (
-    <div>
-      <h1 className='text-3xl font-bold underline'>App</h1>
-    </div>
+    <BrowserRouter>
+
+    <Header>
+    <Routes>
+      <Route path='/' element=  {<Home></Home>} />
+      <Route path='/sign-in' element=  {<SignIn></SignIn>} />
+      <Route path='/sign-up' element=  {<SignUp></SignUp>} />
+      <Route path='/about' element=  {<About></About>} />
+    
+   
+    </Routes>
+
+    </Header>
+    </BrowserRouter>
   )
 }
 
